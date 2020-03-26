4d3c8bb411efbf8d38b679028617f0916137a0c1
Nitya Tarakad <nitarakad@gmail.com>
2020-03-25 18:05:17 -0700

    retrieve readable data

--- HelpOthers/ListUsersHelpViewController.swift
+++ HelpOthers/ListUsersHelpViewController.swift
@@ -19,12 +19,61 @@ class ListUsersHelpViewController: UIViewController {
         
         databaseRef = Database.database().reference()
         
-        var dictHere = [String: AnyObject]()
-        
         let usersWantHelp = databaseRef.observe(.value) { (snapshot) in
             let allUsers = snapshot.value as? [String: AnyObject] ?? [:]
             let usersWantHelp = allUsers["wantHelp_user"] as! Dictionary<String, Dictionary<String, String>>
-            print(usersWantHelp)
+            var userUIDs = Array<String>()
+            var usernames = Dictionary<String, String>()
+            var addresses = Dictionary<String, String>()
+            var listItems = Dictionary<String, String>()
+            var helpWiths = Dictionary<String, String>()
+            var timeOfDeliveries = Dictionary<String, String>()
+            for userInfo in usersWantHelp {
+                let currUID = userInfo.key
+                userUIDs.append(currUID)
+                guard let info = usersWantHelp[userInfo.key] else {
+                    print("no users want help")
+                    return
+                }
+                
+                guard let username = info["username"] else {
+                    print("no username saved")
+                    return
+                }
+                
+                guard let wantHelpWith = info["want_help_with"] else {
+                    print("no category of help saved")
+                    return
+                }
+                
+                guard let listOfItems = info["list_of_items"] else {
+                    print("no list of items needed saved")
+                    return
+                }
+                
+                guard let timeOfDelivery = info["time_of_delivery"] else {
+                    print("no time of delivery saved")
+                    return
+                }
+                
+                guard let address = info["address"] else {
+                    print("no address saved")
+                    return
+                }
+                
+                usernames[currUID] = username
+                helpWiths[currUID] = wantHelpWith
+                listItems[currUID] = listOfItems
+                timeOfDeliveries[currUID] = timeOfDelivery
+                addresses[currUID] = address
+            }
+            
+            print(usernames)
+            print(helpWiths)
+            print(listItems)
+            print(timeOfDeliveries)
+            print(addresses)
+            
         }
         
     }
