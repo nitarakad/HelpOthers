deff83edcd47d95ffb9ef24324751840fb41a399
Nitya Tarakad <nitarakad@gmail.com>
2020-03-26 19:22:43 -0700

    auto layout of want help screen

--- HelpOthers/WantHelpViewController.swift
+++ HelpOthers/WantHelpViewController.swift
@@ -15,6 +15,9 @@ class WantHelpViewController: UIViewController {
     @IBOutlet weak var nameInputField: UITextField!
     @IBOutlet weak var groceriesButton: UIButton!
     @IBOutlet weak var prescriptionButton: UIButton!
+    @IBOutlet weak var helloThereLabel: UILabel!
+    @IBOutlet weak var nameQuestionLabel: UILabel!
+    @IBOutlet weak var helpWithLabel: UILabel!
     
     static var userName = ""
     static var userUUID = ""
@@ -24,6 +27,55 @@ class WantHelpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // helloThereLabel auto layout
+        helloThereLabel.translatesAutoresizingMaskIntoConstraints = false
+        helloThereLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        helloThereLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        helloThereLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        helloThereLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        helloThereLabel.textAlignment = .center
+        
+        // nameQuestionLabel auto layout
+        nameQuestionLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameQuestionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameQuestionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        nameQuestionLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        nameQuestionLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        nameQuestionLabel.font = UIFont.systemFont(ofSize: 35)
+        nameQuestionLabel.textAlignment = .center
+        
+        // nameInputField auto layout
+        nameInputField.translatesAutoresizingMaskIntoConstraints = false
+        nameInputField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameInputField.topAnchor.constraint(equalTo: view.topAnchor, constant: 230).isActive = true
+        nameInputField.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        nameInputField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        nameInputField.textAlignment = .center
+        nameInputField.adjustsFontSizeToFitWidth = true
+        
+        // helpWithLabel auto layout
+        helpWithLabel.translatesAutoresizingMaskIntoConstraints = false
+        helpWithLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        helpWithLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 325).isActive = true
+        helpWithLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        helpWithLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        helpWithLabel.textAlignment = .center
+        
+        // groceriesButton auto layout
+        groceriesButton.translatesAutoresizingMaskIntoConstraints = false
+        groceriesButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        groceriesButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 400).isActive = true
+        groceriesButton.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        groceriesButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        // prescriptionButton auto layout
+        prescriptionButton.translatesAutoresizingMaskIntoConstraints = false
+        prescriptionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        prescriptionButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 475).isActive = true
+        prescriptionButton.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        prescriptionButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
         nameInputField.delegate = self
         
         databaseRef = Database.database().reference()
