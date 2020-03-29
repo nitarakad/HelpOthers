afc524ad108d191379181a56760572f0e853ab37
Nitya Tarakad <nitarakad@gmail.com>
2020-03-28 21:41:28 -0700

    ui for selected user screen

--- HelpOthers/SelectedUserViewController.swift
+++ HelpOthers/SelectedUserViewController.swift
@@ -12,7 +12,10 @@ import Firebase
 
 class SelectedUserViewController: UIViewController {
     
+    @IBOutlet weak var thanksLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var listItemsLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var itemsTextView: UITextView!
     @IBOutlet weak var retrievedButton: UIButton!
     @IBOutlet weak var readyButton: UIButton!
@@ -23,10 +26,10 @@ class SelectedUserViewController: UIViewController {
     static var currentStatus = "none"
     
     override func viewDidLoad() {
-        
-        // TODO: display address
-        
         super.viewDidLoad()
+        
+        let buttonBackgroundNormal = UIImage(named: "regular_button_bkgd")
+        let buttonBackgroundNormalClicked = UIImage(named: "regular_button_clicked_bkgd")
         
         databaseRef = Database.database().reference()
         
@@ -36,22 +39,94 @@ class SelectedUserViewController: UIViewController {
         readyButton.isEnabled = false
         returnToMainScreen.isEnabled = false
         
-        let nameWithColon = ListUsersHelpViewController.buttonSelectedName
-        let lengthJustName = nameWithColon.count - 2
-        let index = nameWithColon.index(nameWithColon.startIndex, offsetBy: lengthJustName)
-        let name = nameWithColon[..<index]
+        let name = ListUsersHelpViewController.buttonSelectedName
         
         nameLabel.text = "\(String(name))!"
         nameLabel.adjustsFontSizeToFitWidth = true
         
         itemsTextView.isScrollEnabled = true
-        itemsTextView.center.x = self.view.center.x
-        itemsTextView.layer.borderColor = CGColor(genericGrayGamma2_2Gray: 1.0, alpha: 1.0)
-        itemsTextView.layer.borderWidth = 1.0
-        itemsTextView.textColor = UIColor.gray
         itemsTextView.font = UIFont.systemFont(ofSize: 30)
         itemsTextView.isEditable = false
         self.view.addSubview(itemsTextView)
+        
+        /* AUTO LAYOUT*/
+        thanksLabel.translatesAutoresizingMaskIntoConstraints = false
+        thanksLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        thanksLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        thanksLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        thanksLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        thanksLabel.textAlignment = .center
+        thanksLabel.adjustsFontSizeToFitWidth = true
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 140).isActive = true
+        nameLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        nameLabel.textAlignment = .center
+        nameLabel.adjustsFontSizeToFitWidth = true
+        
+        listItemsLabel.translatesAutoresizingMaskIntoConstraints = false
+        listItemsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        listItemsLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 190).isActive = true
+        listItemsLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        listItemsLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        listItemsLabel.textAlignment = .center
+        listItemsLabel.adjustsFontSizeToFitWidth = true
+        
+        itemsTextView.translatesAutoresizingMaskIntoConstraints = false
+        itemsTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        itemsTextView.topAnchor.constraint(equalTo: view.topAnchor, constant: 260).isActive = true
+        itemsTextView.widthAnchor.constraint(equalToConstant: 360).isActive = true
+        itemsTextView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        itemsTextView.textAlignment = .left
+        itemsTextView.layer.borderColor = CGColor(srgbRed: 58/255, green: 193/255, blue: 236/255, alpha: 1.0)
+        itemsTextView.layer.borderWidth = 2.0
+
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        addressLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        addressLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 570).isActive = true
+        addressLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        addressLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        addressLabel.textAlignment = .center
+        addressLabel.adjustsFontSizeToFitWidth = true
+        addressLabel.lineBreakMode = .byClipping
+        addressLabel.numberOfLines = 3
+        
+        retrievedButton.translatesAutoresizingMaskIntoConstraints = false
+        retrievedButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        retrievedButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 660).isActive = true
+        retrievedButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        retrievedButton.heightAnchor.constraint(equalToConstant: 90).isActive = true
+        retrievedButton.titleLabel?.textAlignment = .center
+        retrievedButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        retrievedButton.setBackgroundImage(buttonBackgroundNormal, for: .normal)
+        retrievedButton.setBackgroundImage(buttonBackgroundNormalClicked, for: .highlighted)
+        retrievedButton.setTitleColor(UIColor(red: 227/255, green: 227/255, blue: 1.0, alpha: 1.0), for: .normal)
+
+        readyButton.translatesAutoresizingMaskIntoConstraints = false
+        readyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        readyButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 660).isActive = true
+        readyButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        readyButton.heightAnchor.constraint(equalToConstant: 90).isActive = true
+        readyButton.titleLabel?.textAlignment = .center
+        readyButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        readyButton.isHidden = true
+        readyButton.setBackgroundImage(buttonBackgroundNormal, for: .normal)
+        readyButton.setBackgroundImage(buttonBackgroundNormalClicked, for: .highlighted)
+        readyButton.setTitleColor(UIColor(red: 227/255, green: 227/255, blue: 1.0, alpha: 1.0), for: .normal)
+        
+        returnToMainScreen.translatesAutoresizingMaskIntoConstraints = false
+        returnToMainScreen.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        returnToMainScreen.topAnchor.constraint(equalTo: view.topAnchor, constant: 660).isActive = true
+        returnToMainScreen.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        returnToMainScreen.heightAnchor.constraint(equalToConstant: 90).isActive = true
+        returnToMainScreen.titleLabel?.textAlignment = .center
+        returnToMainScreen.titleLabel?.adjustsFontSizeToFitWidth = true
+        returnToMainScreen.isHidden = true
+        returnToMainScreen.setBackgroundImage(buttonBackgroundNormal, for: .normal)
+        returnToMainScreen.setBackgroundImage(buttonBackgroundNormalClicked, for: .highlighted)
+        returnToMainScreen.setTitleColor(UIColor(red: 227/255, green: 227/255, blue: 1.0, alpha: 1.0), for: .normal)
         
         let observeDB = databaseRef.child("wantHelp_user").child(ListUsersHelpViewController.buttonSelectedUUID).observeSingleEvent(of: .value) { (snapshot) in
             
@@ -62,8 +137,20 @@ class SelectedUserViewController: UIViewController {
                 return
             }
             
+            guard let address = allInfo["address"] else {
+                print("no address found")
+                return
+            }
+            
+            guard let name = allInfo["username"] else {
+                print("no name found")
+                return
+            }
             print("**items in textview now displayed**")
             self.itemsTextView.text = listOfItems
+            let addressLabelText = "\(name)'s Address: \(address)"
+            self.addressLabel.text = addressLabelText
+
         }
         
     }
@@ -74,6 +161,9 @@ class SelectedUserViewController: UIViewController {
         updateHelpUserDatabaseWithRetrieved()
         print("items retreived!")
         readyButton.isEnabled = true
+        readyButton.isHidden = false
+        retrievedButton.isEnabled = false
+        retrievedButton.isHidden = true
     }
     
     @IBAction func readyButtonClicked(_ sender: Any) {
@@ -82,6 +172,9 @@ class SelectedUserViewController: UIViewController {
         updateWantHelpUserDatabaseWithDelivered()
         print("item at doorstep!")
         returnToMainScreen.isEnabled = true
+        returnToMainScreen.isHidden = false
+        readyButton.isEnabled = false
+        readyButton.isHidden = true
     }
     
     @IBAction func mainScreenButtonClicked(_ sender: Any) {
